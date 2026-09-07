4760b469cc7608e76003cdd77ac204d8e4b70ad0
SamuelMaria <287851107+SamuelMaria@users.noreply.github.com>
2026-09-07 17:05:23 -0600

    Se terminó el proceso de subir la imagen del contenedor del proyecto de Django y Docker

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -1,8 +1,9 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 urlpatterns = [
+    path('', RedirectView.as_view(url='/polls/', permanent=False)),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls()
